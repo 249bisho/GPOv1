15e6076c00d5b6cca1e6d836cf696380246b2677
بشير عبدالحليم المبارك الشيخ <abdulhalimbasheer@gmail.com>
2026-01-29 23:44:10 +0200

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,12 @@
-export default function NotFound() {
+import Head from 'next/head'
+
+// Inside your function:
+<Head>
+  <title>Bisho | Global CX & BPO Solutions</title>
+  <meta name="description" content="Premium multilingual BPO services for U.S. and Middle East markets." />
+  <link rel="icon" href="/favicon.ico" />
+</Head>
+  export default function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
